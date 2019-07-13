0e11c1f1b2dc1462f944458359009cec38023382
Seriq <e.schlarmann@posteo.de>
2019-07-13 23:05:52 +0200

    chapter 8 done

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,7 @@ if not app.debug:
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
                                        backupCount=5)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %([pathname)s:%(lineno)d]'))
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
