64baae1ff9df3a0d2fe083b5f5f4c630d28ff21f
Seriq <e.schlarmann@posteo.de>
2019-07-13 16:31:11 +0200

    chapt0r 6

--- app/models.py
+++ app/models.py
@@ -3,12 +3,15 @@ from datetime import datetime
 from app import db
 from app import login
 from flask_login import UserMixin
+from hashlib import md5
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(256))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', 
                             backref='author', 
                             lazy='dynamic')
@@ -21,6 +24,11 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
